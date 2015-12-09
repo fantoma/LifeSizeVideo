f7824b3e50e3e37b83d1546554568552a64e0064
Diego Fantoma <diego@fantoma.it>
2015-12-09 11:35:14 +0100

    db/install.php

--- db/access.php
+++ db/access.php
@@ -31,10 +31,7 @@ $capabilities = array(
         'captype' => 'read',
         'contextlevel' => CONTEXT_MODULE,
         'archetypes' => array(
-            'coursecreator' => CAP_ALLOW,
-            'teacher' => CAP_ALLOW,
-            'editingteacher' => CAP_ALLOW,
-            'manager' => CAP_ALLOW
+            'user' => CAP_ALLOW
         )
     )
 );
--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015051103;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2015051104;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2015050500;        // Requires this Moodle version.
 $plugin->component = 'repository_lifesizevideo'; // Full name of the plugin (used for diagnostics).
 $plugin->cron = 3600; // How often to run automatic updates (0 to disable)
